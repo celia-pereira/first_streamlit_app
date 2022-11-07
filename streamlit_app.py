f20167b0a2cee8790ed483cc030506d1316d5289
Célia Pereira <113099095+celia-pereira@users.noreply.github.com>
2022-11-07 17:50:58 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,22 +22,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-#streamlit.dataframe(my_fruit_list)
-
 streamlit.dataframe(fruits_to_show)
 
-# create the repetable code block (called a function)
-def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
 
-#New section to display frutyvice api response
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-  fruit_choice = streamlit.text_input("what fruit would you like infromation about?")
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to get infromation")
-  else:
-    back_from_function = get_fruityvice_data(fruit_choice)
